a896ec0b46a4613f5836ee3f1aba3ae1b74ad374
shaileshwebdeveloper <shailesh.webdevelopment@gmail.com>
2022-05-16 22:29:19 +0530

    assignment done

---
+++ src/components/mobile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Mobile = () =>{
+    return <div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>Windows Phone</li>
+        </ul>
+       </div>
+};
+
+export default Mobile;
+
